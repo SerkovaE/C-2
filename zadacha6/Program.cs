423cab034ed1673e3aa742bb13dffa1ed9311c82
SerkovaE <katti107@mail.ru>
2023-03-30 20:11:25 +0300

    add end6

---
+++ zadacha6/Program.cs
@@ -0,0 +1,11 @@
+﻿int numberA;
+Console.WriteLine("Введите число.");
+numberA = Convert.ToInt32(Console.ReadLine());
+if (numberA%2==0)
+{
+    Console.Write("число является четным");
+}
+else 
+{
+    Console.Write("число не является четным");
+}
