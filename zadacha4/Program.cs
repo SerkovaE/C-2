db21e9b64488b16a06784bf9973137b1bc22624e
SerkovaE <katti107@mail.ru>
2023-03-30 19:54:23 +0300

    add first

---
+++ zadacha4/Program.cs
@@ -0,0 +1,15 @@
+﻿int a;
+int b;
+int c;
+int max=a;
+Console.WriteLine("Введите три числа.");
+a = Convert.ToInt32(Console.ReadLine());
+b = Convert.ToInt32(Console.ReadLine());
+с = Convert.ToInt32(Console.ReadLine());
+if (b>max)
+{
+    max=b;
+}
+if (c>max){max=c}
+
+Console.WriteLine(max);
